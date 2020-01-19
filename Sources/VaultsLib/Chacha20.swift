71b1f244bd892b6fc5b46c7a6b0959f9ad1cc360
Asaf Niv <asafniv511@icloud.com>
2020-01-19 14:52:19 +0200

    Add comments

--- Sources/VaultsLib/Chacha20.swift
+++ Sources/VaultsLib/Chacha20.swift
@@ -12,11 +12,11 @@ let ivLen = 12
 let tagLen = 16
 let metadataLen = authHeaderLen+ivLen+tagLen
 func encryptData(password: String, message: Data) -> Array<UInt8>? {
-    let iv = ChaCha20.randomIV(ivLen)
+    let iv = ChaCha20.randomIV(ivLen) // Make a random IV
     do {
-        let authHeader = randomAuthHeader()
+        let authHeader = randomAuthHeader() // Random auth header for Poly1305 (verification)
         let (encryptedData, tag) = try AEADChaCha20Poly1305.encrypt(message.bytes, key: makePassword(plaintextPass: password).bytes, iv: iv, authenticationHeader: authHeader)
-        return iv+authHeader+tag+encryptedData
+        return iv+authHeader+tag+encryptedData // Encrypted block structure, authHeader and tag are needed to verify our data
     } catch  {
         print("Failed to encrypt data")
     }
@@ -27,21 +27,26 @@ func decryptData(password: String, message: Data) -> Array<UInt8>? {
     var ciphertext: Array<UInt8> = []
     var authHeader: Array<UInt8> = []
     var tag: Array<UInt8> = []
+    // Extract IV, first 12 bytes
     while iv.count < ivLen {
         iv.append(message[iv.count])
     }
+    // Extract authHeader, 64 bytes after the IV
     while authHeader.count < authHeaderLen {
         authHeader.append(message[authHeader.count+ivLen])
     }
+    // Extract tag, 16 bytes after the authHeader
     while tag.count < tagLen {
         tag.append(message[ivLen+authHeaderLen+tag.count])
     }
+    // Finally filter out ALL metadata and extract encryptedData (got returned from encryptData)
     while ciphertext.count < message.count-metadataLen {
         ciphertext.append(message[ivLen+tagLen+authHeaderLen+ciphertext.count])
     }
     do {
+        // Decrypt it
         var (decryptedData, success) = try AEADChaCha20Poly1305.decrypt(ciphertext, key: makePassword(plaintextPass: password).bytes, iv: iv, authenticationHeader: authHeader, authenticationTag: tag)
-        if !success {
+        if !success { // If it failed, data was modified
             decryptedData = []
             print("either nsa modified this data or your storage is fucked")
             return nil
--- Sources/VaultsLib/VaultAccess.swift
+++ Sources/VaultsLib/VaultAccess.swift
@@ -8,22 +8,27 @@
 import Foundation
 let mbsize = 1048576
 let blocksize = 64*mbsize
+let encryptedBlockLen = blocksize+metadataLen
 public class VaultAccess {
     public static func addFile(pathToAdd: String, vaultPath: String, pathInVault: String, pass: String) {
         // TODO: UUID parser should kick in
-        let fileToAdd = FileHandle.init(forReadingAtPath: pathToAdd)
-        FileManager.default.createFile(atPath: vaultPath+"/"+pathInVault, contents: nil, attributes: nil)
-        let fileToWrite = FileHandle.init(forWritingAtPath: vaultPath+"/"+pathInVault)
-        let fileSize = getFileSize(url: pathToAdd)
-        var i: UInt64 = 0
+        let fileToAdd = FileHandle.init(forReadingAtPath: pathToAdd) // Open a file handle for reading
+        FileManager.default.createFile(atPath: vaultPath+"/"+pathInVault, contents: nil, attributes: nil) // Create the file we're going to write to
+        let fileToWrite = FileHandle.init(forWritingAtPath: vaultPath+"/"+pathInVault) // Open a file handle for that file
+        let fileSize = getFileSize(url: pathToAdd) // Get the file size of the file we're reading
+        var i: UInt64 = 0 // Basically our current offset in the file we're reading
         while fileSize > i {
-            fileToAdd?.seek(toFileOffset: i)
-            let block = fileToAdd?.readData(ofLength: blocksize)
+            // Iterate over blocks in the file
+            fileToAdd?.seek(toFileOffset: i) // Move file pointer after the block we just read
+            let block = fileToAdd?.readData(ofLength: blocksize) // Read 64MB from that file pointer
             let encryptedBlock = encryptData(password: pass, message: block!)
-            fileToWrite?.write(Data(bytes: encryptedBlock!, count: encryptedBlock!.count))
-            fileToWrite?.seekToEndOfFile()
+            fileToWrite?.write(Data(bytes: encryptedBlock!, count: encryptedBlock!.count)) // Write data to file
+            fileToWrite?.seekToEndOfFile() // Move to the end of that file so we'll append to it and not overwrite anything
             print("encrypted block")
             i += UInt64(blocksize)
+            // close files
+            fileToWrite?.closeFile()
+            fileToAdd?.closeFile()
         }
     }
     public static func getFile(vaultPath: String, pathInVault: String, pass: String) {
@@ -35,13 +40,15 @@ public class VaultAccess {
         // Get file size of the encrypted file
         let fileSize = getFileSize(url: toDecrypt)
         while fileSize > i {
-            fileToGet?.seek(toFileOffset: i)
-            let block = (fileToGet?.readData(ofLength: blocksize+metadataLen))!
-            let decryptedBlock = decryptData(password: pass, message: block)
+            fileToGet?.seek(toFileOffset: i) // Move to the correct location in the file
+            let block = (fileToGet?.readData(ofLength: encryptedBlockLen))! // Read an encrypted block from it (bigger than normal block cuz metadata)
+            let decryptedBlock = decryptData(password: pass, message: block) // Decrypt the encrypted block
             testfile?.write(Data(bytes: decryptedBlock!, count: decryptedBlock!.count))
             testfile?.seekToEndOfFile()
-            i += UInt64(blocksize+metadataLen)
+            i += UInt64(encryptedBlockLen)
             print("decrypted block")
+            testfile?.closeFile()
+            fileToGet?.closeFile()
         }
  }
         static func getFileSize(url: String)-> UInt64  {
